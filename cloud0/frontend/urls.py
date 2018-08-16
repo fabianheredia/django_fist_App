5016a4309ee285d0033c39ad431191e0d9548c3d
fabian <fabian.heredia@gmail.com>
2018-08-16 17:33:35 -0500

    Adicion de alunos estilos de diseño

--- cloud0/frontend/urls.py
+++ cloud0/frontend/urls.py
@@ -1,6 +1,6 @@
 from . import views as frontendview
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import url, include, static
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 urlpatterns = [
